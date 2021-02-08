922bed7a0798e86895dcd8919f55d45fe19d48cb
arxdust <u.iaroslav@gmail.com>
2021-02-08 12:18:00 +0300

    fix main.js

--- Seafarer/test.js
+++ Seafarer/test.js
@@ -1,3 +1,4 @@
 
+const mydata = JSON.parse(document.getElementById('mydata').textContent)
 let headDiscription = document.querySelector('.news-box__header-title h2');
 headDiscription.innerHTML = mydata;
