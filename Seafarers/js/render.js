1912d39d3c89b18942bda83dfe03835a8cb7d9f3
arxdust <u.iaroslav@gmail.com>
2021-02-09 01:26:04 +0300

    not load with ../path

--- Seafarers/js/render.js
+++ Seafarers/js/render.js
@@ -1,26 +1,26 @@
-windows.addEventListener('load', function () {
-    let renderNewsH1 = document.querySelector('.news-render h1');
-    let renderNewsH2 = document.querySelector('.news-render h2');
-    let renderText = document.querySelector('.news-box__body-text');
+// windows.addEventListener('load', function () {
+//     let renderNewsH1 = document.querySelector('.news-render h1');
+//     let renderNewsH2 = document.querySelector('.news-render h2');
+//     let renderText = document.querySelector('.news-box__body-text');
+//
+//     function render(item) {
+//         let div = document.createElement('div');
+//         let txt = null;
+//
+//         div.innerHTML = item;
+//         txt = div.innerText;
+//         return txt;
+//     }
+//
+//     renderNewsH1.innerHTML = render(title);
+//     renderNewsH2.innerHTML = render(description);
+//     renderText.innerHTML = render(articles);
+// })
 
-    function render(item) {
-        let div = document.createElement('div');
-        let txt = null;
+let txtDiv = document.querySelector('.news-box__body-text');
+let newDiv = document.createElement('div')
+let txt = null;
 
-        div.innerHTML = item;
-        txt = div.innerText;
-        return txt;
-    }
-
-    renderNewsH1.innerHTML = render(title);
-    renderNewsH2.innerHTML = render(description);
-    renderText.innerHTML = render(articles);
-    //
-    // let txtDiv = document.querySelector('.news-box__body-text');
-    // let newDiv = document.createElement('div')
-    // let txt = null;
-    //
-    // newDiv.innerHTML = `{{ full_data.articles }}`;
-    // txt = newDiv.innerText;
-    // txtDiv.innerHTML = txt;
-})
+newDiv.innerHTML = `{{ full_data.articles }}`;
+txt = newDiv.innerText;
+txtDiv.innerHTML = txt;
