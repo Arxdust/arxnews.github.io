0c0c505e49f112eec573eb2fc9dfe85ed7c983d1
arxdust <u.iaroslav@gmail.com>
2021-02-09 01:12:20 +0300

    not load with ../path

--- Seafarers/js/render.js
+++ Seafarers/js/render.js
@@ -2,6 +2,6 @@ let txtDiv = document.querySelector('.news-box__body-text');
 let newDiv = document.createElement('div')
 let txt = null;
 
-newDiv.innerHTML = `mydata`;
+newDiv.innerHTML = `{{ full_data.articles }}`;
 txt = newDiv.innerText;
 txtDiv.innerHTML = txt;
