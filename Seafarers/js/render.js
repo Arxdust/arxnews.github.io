79e813312cfe810b9896950aa1fd1456998986e1
arxdust <u.iaroslav@gmail.com>
2021-02-09 01:28:02 +0300

    not load with ../path

--- Seafarers/js/render.js
+++ Seafarers/js/render.js
@@ -21,6 +21,6 @@ let txtDiv = document.querySelector('.news-box__body-text');
 let newDiv = document.createElement('div')
 let txt = null;
 
-newDiv.innerHTML = `{{ full_data.articles }}`;
+newDiv.innerHTML = articles;
 txt = newDiv.innerText;
 txtDiv.innerHTML = txt;
