2d9a68780dd4a35210ea4772249e365270406bd3
X <sersergiusx@gmail.com>
2021-02-10 04:54:38 +0200

    render newsBoxFooter

--- Seafarers/js/render.js
+++ Seafarers/js/render.js
@@ -5,7 +5,6 @@ window.addEventListener('load', function () {
     let renderText = document.querySelector('.news-box__body-text');
     let preRenderText = renderText;
     let newsRender = document.querySelector('.news-render');
-    let newsBoxFooter = document.createElement('div');
 
     function render(item) {
         let div = document.createElement('div');
@@ -20,17 +19,33 @@ window.addEventListener('load', function () {
     renderNewsH1.innerHTML = render(title);
     renderNewsH2.innerHTML = render(description);
     renderText.innerHTML = render(articles);
-    newsBoxFooter.classList.add('news-box__footer');
-    newsRender.append(newsBoxFooter);
 
-    renderText.childNodes.forEach(function(item) {
-        if (item.tagName == "DIV") {
-            let newItem = item;
+    scanRenderText();
 
-            item.remove();
-            newsBoxFooter.append(newItem);
+    function scanRenderText() {
+        for (let key in renderText.childNodes) {
+            if (key.tagName == "DIV") {
+                renderNewsBoxFooter();
+                break;
+            }
         }
-    });
+    }
+
+    function renderNewsBoxFooter() {
+        let newsBoxFooter = document.createElement('div');
+
+        renderText.childNodes.forEach(function(item) {
+            if (item.tagName == "DIV") {
+                let newItem = item;
+
+                item.remove();
+                newsBoxFooter.append(newItem);
+            }
+        });
+
+        newsBoxFooter.classList.add('news-box__footer');
+        newsRender.append(newsBoxFooter);
+    }
 
     newsBoxBody.append(preRenderText);
 });
