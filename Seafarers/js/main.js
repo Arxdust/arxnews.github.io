2391a7c2cfba496bf230fcacacf9035928a17d5a
X <sersergiusx@gmail.com>
2021-02-20 01:51:56 +0200

    render comment, add link, delete style, fix

--- Seafarers/js/main.js
+++ Seafarers/js/main.js
@@ -1,6 +1,3 @@
-let invite = 0;
-let inviteLoad = 0;
-
 // pre-load css
 function addstyle() {
     const link = document.createElement('link'),
@@ -43,54 +40,6 @@ window.addEventListener('load', function() {
         sessionStorage.setItem('animation', 'done');
     });
 
-    // view-img
-    const newsBoxHeaderImg = document.querySelector('.news-box__header-img');
-
-    newsBoxHeaderImg.addEventListener('click', function(){
-        newsBoxHeaderImg.classList.toggle('view');
-        body.classList.toggle('hidden');
-
-        if (document.querySelector(".news-box__header-img").classList.contains("view")) {
-            newsBoxImg.classList.remove('zoom');
-        }
-    })
-
-    // zoom-img
-    const newsBoxImg = document.querySelector('.news-box__img');
-
-    newsBoxHeaderImg.addEventListener('mouseover',
-    function(){
-        if (document.querySelector(".news-box__header-img").classList.contains("view") == false) {
-            newsBoxImg.classList.add('zoom');
-            newsBoxImg.classList.remove('zoomout');
-        } else {
-            newsBoxImg.classList.remove('zoom');
-        }
-    });
-
-    newsBoxHeaderImg.addEventListener('mouseout',
-    function(){
-        if (document.querySelector(".news-box__header-img").classList.contains("view") == false) {
-            newsBoxImg.classList.add('zoomout');
-            newsBoxImg.classList.remove('zoom');
-        } else {
-            newsBoxImg.classList.remove('zoom');
-        }
-    });
-
-    // autosize textarea
-    let tx = document.querySelectorAll('.textarea');
-
-    for (let i = 0; i < tx.length; i++) {
-        tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px');
-        tx[i].addEventListener("input", OnInput, false);
-    }
-
-    function OnInput() {
-        this.style.height = 'auto';
-        this.style.height = (this.scrollHeight) + 'px';
-    }
-
     // footer
     const footer = document.querySelector('.footer .container'),
         footerBox = document.createElement('ul'),
@@ -129,57 +78,6 @@ window.addEventListener('load', function() {
         li.append(a);
         footerBox.append(li);
     });
-
-    // last news
-    const newsMenu = document.querySelector('.news-menu'),
-        preImg = 'https://arxdust.github.io/arxnews.github.io/Seafarers/images/pre-img.jpg',
-        newsUl = document.createElement('ul');
-
-    renderBackendLastNews(lastNews);
-
-    function renderBackendLastNews(arr) {
-        if (Array.isArray(arr)) {
-            renderLastNews(arr);
-        }
-    }
-
-    function renderLastNews(arr) {
-        arr.forEach(function(item) {
-            let li = document.createElement('li'),
-                title = document.createElement('span'),
-                divImg = document.createElement('div'),
-                divBg = document.createElement('div'),
-                a = document.createElement('a'),
-                imgUrl = item['img'];
-
-            typeof item['title'] == 'string' ? title.innerHTML = item['title'] : title.innerHTML = 'Title';
-
-            checkUrlImg(imgUrl).then( result => {divImg.style.backgroundImage = `url(${imgUrl})`}, error => {divImg.style.backgroundImage = `url(${preImg})`})
-
-            divImg.classList.add('pre-img');
-            a.href = item['url'];
-            a.setAttribute('title', '');
-            a.classList.add('news-link');
-
-            a.append(title, divBg, divImg);
-            li.append(a);
-            newsUl.append(li);
-        });
-
-        newsUl.classList.add('unselect');
-        newsMenu.append(newsUl);
-    }
-
-    // checkUrlImg
-    function checkUrlImg(imgUrl) {
-        return new Promise((resolve, reject) => {
-            let img = new Image();
-            img.src = imgUrl;
-
-            img.onload = () => resolve(imgUrl);
-            img.onerror = () => reject(new Error(`Img undefined: ${imgUrl}`));
-        });
-    }
 
     //header-nav
     const headerNav = document.querySelector('.header-nav'),
@@ -239,47 +137,4 @@ window.addEventListener('load', function() {
             localStorage.setItem('theme', 'dark');
         }
     })
-
-    // text-range
-    const textRange = document.querySelector('.text-range'),
-        newsTitle = document.querySelector('.news-box__header-title h1'),
-        newsDescription = document.querySelector('.news-box__header-title h2'),
-        newsText = document.querySelector('.news-box__body-text');
-
-    if (typeof localStorage.getItem('textRange') == "string") {
-        textRange.valueAsNumber = parseInt(localStorage.getItem('textRange'))
-    } else {
-        textRange.valueAsNumber = 0;
-    }
-
-    let countTextRange = textRange.valueAsNumber;
-
-    fTextRange();
-
-    textRange.addEventListener('input', function(event) {
-        countTextRange = event.target.valueAsNumber;
-        fTextRange();
-    });
-
-    textRange.addEventListener('mouseup', lStorageText);
-    textRange.addEventListener('touchend', lStorageText);
-
-    function lStorageText() {
-        localStorage.setItem('textRange', countTextRange);
-    }
-
-    function fTextRange() {
-        newsTitle.style.fontSize = 1.5 + 1.5 / 100 * countTextRange + 'rem';
-        newsTitle.style.lineHeight = 1.7 + 1.7 / 100 * countTextRange + 'rem';
-        newsTitle.style.marginBottom = 15 + 15 / 100 * countTextRange + 'px';
-        newsDescription.style.fontSize = 1.2 + 1.2 / 100 * countTextRange + 'rem';
-        newsDescription.style.lineHeight = 1.4 + 1.4 / 100 * countTextRange + 'rem';
-        newsDescription.style.marginBottom = 15 + 15 / 100 * countTextRange + 'px';
-        newsText.style.fontSize = 1 + 1 / 100 * countTextRange + 'rem';
-        newsText.style.lineHeight = 1.4 + 1.4 / 100 * countTextRange + 'rem';
-    }
-
-    inviteLoad += 1;
 });
-
-invite += 1;
