8c9dbfb30b25096ebb207027dcf42aa78233fe61
X <sersergiusx@gmail.com>
2021-02-19 17:28:38 +0200

    text margin

--- Seafarers/js/main.js
+++ Seafarers/js/main.js
@@ -93,7 +93,6 @@ window.addEventListener('load', function() {
         newsDescription.style.marginBottom = 15 + 15 / 100 * countTextRange + 'px';
         newsText.style.fontSize = 1 + 1 / 100 * countTextRange + 'rem';
         newsText.style.lineHeight = 1.4 + 1.4 / 100 * countTextRange + 'rem';
-        newsTextParagraph.style.marginBottom = 15 + 15 / 100 * countTextRange + 'px';
     }
 
     // animation header
