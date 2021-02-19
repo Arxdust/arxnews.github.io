65c05ebd1c65916925be7d67bb8493f609c7eead
X <sersergiusx@gmail.com>
2021-02-19 12:19:01 +0200

    header-nav click over element close

--- Seafarers/js/main.js
+++ Seafarers/js/main.js
@@ -259,13 +259,14 @@ window.addEventListener('load', function() {
 
     headerNavBtn.classList.add('header-nav-btn');
 
-    headerNavBtn.addEventListener('click', toggleMenu);
+    headerNavBtn.addEventListener('click', () => { toggleMenu() });
 
     document.addEventListener('click', function(event) {
         let target = event.currentTarget,
             its_menu = target == headerNav,
-
             menu_is_active = headerNav.classList.contains('open');
+
+            console.log(target);
 
         if (!its_menu && menu_is_active) {
             toggleMenu();
