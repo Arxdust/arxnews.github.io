d771cfcf507c93eefb9c74007d98d97c542f47f9
X <sersergiusx@gmail.com>
2021-02-20 16:25:38 +0200

    telegram iframe css render

--- Seafarers/js/iframe.js
+++ Seafarers/js/iframe.js
@@ -1,10 +1,9 @@
 window.addEventListener('load', () => {
     let iframe = document.querySelectorAll('iframe'),
-        link = document.createElement('link'),
-        widgetMessageUser = document.querySelectorAll('.tgme_widget_message_user')
-    console.dir('dir:', widgetMessageUser);
+        link = document.createElement('link');
+    console.dir(iframe);
 
-    widgetMessageUser.style.display = 'none';
+    // widgetMessageUser.style.display = 'none';
 
     // link.rel = 'stylesheet';
     // link.href = 'https://arxdust.github.io/arxnews.github.io/Seafarers/css/iframe.css';
