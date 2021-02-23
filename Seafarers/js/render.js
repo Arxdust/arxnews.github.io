033bdbfc171e410518ef20833a97356cfaeb0e09
X <sersergiusx@gmail.com>
2021-02-23 22:09:05 +0200

    fixed

--- Seafarers/js/render.js
+++ Seafarers/js/render.js
@@ -139,17 +139,20 @@ window.addEventListener('load', function () {
 
         // comment link
         const commentThisBoxLink = document.querySelector('.comment-this-box a'),
-            commentThisBoxLinkText = document.createElement('span');
+            commentThisBoxLinkSpan = document.createElement('span'),
+            commentTHisBoxLinkSvg = `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="comment-alt" class="svg-inline--fa fa-comment-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M448 0H64C28.7 0 0 28.7 0 64v288c0 35.3 28.7 64 64 64h96v84c0 9.8 11.2 15.5 19.1 9.7L304 416h144c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64z"></path></svg>`,
+            commentThisBoxLinkText = 'Write a comment';
 
 
-        commentThisBoxLinkText.innerText = 'Write a comment';
-        commentThisBoxLink.append(commentThisBoxLinkText);
-        commentThisBoxLink.innerHTML += `<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="telegram-plane" class="svg-inline--fa fa-telegram-plane fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M446.7 98.6l-67.6 318.8c-5.1 22.5-18.4 28.1-37.3 17.5l-103-75.9-49.7 47.8c-5.5 5.5-10.1 10.1-20.7 10.1l7.4-104.9 190.9-172.5c8.3-7.4-1.8-11.5-12.9-4.1L117.8 284 16.2 252.2c-22.1-6.9-22.5-22.1 4.6-32.7L418.2 66.4c18.4-6.9 34.5 4.1 28.5 32.2z"></path></svg>`
+        commentThisBoxLinkSpan.insertAdjacentHTML('beforeEnd', commentThisBoxLinkText);
+        commentThisBoxLinkSpan.insertAdjacentHTML('beforeEnd', commentThisBoxLinkSpan);
+        commentThisBoxLinkSpan.insertAdjacentHTML('afterBegin', commentTHisBoxLinkSvg);
 
         // tab-menu-btn
-        const btnComment = document.querySelector('.btn-comment');
+        const btnComment = document.querySelector('.btn-comment'),
+            btnCommentSvg = `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="comment" class="svg-inline--fa fa-comment fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 32C114.6 32 0 125.1 0 240c0 49.6 21.4 95 57 130.7C44.5 421.1 2.7 466 2.2 466.5c-2.2 2.3-2.8 5.7-1.5 8.7S4.8 480 8 480c66.3 0 116-31.8 140.6-51.4 32.7 12.3 69 19.4 107.4 19.4 141.4 0 256-93.1 256-208S397.4 32 256 32z"></path></svg>`
 
-        btnComment.innerHTML += `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="comment" class="svg-inline--fa fa-comment fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 32C114.6 32 0 125.1 0 240c0 49.6 21.4 95 57 130.7C44.5 421.1 2.7 466 2.2 466.5c-2.2 2.3-2.8 5.7-1.5 8.7S4.8 480 8 480c66.3 0 116-31.8 140.6-51.4 32.7 12.3 69 19.4 107.4 19.4 141.4 0 256-93.1 256-208S397.4 32 256 32z"></path></svg>`
+        btnComment.insertAdjacentHTML('afterBegin', btnCommentSvg);
     }
 
     // last news
@@ -199,4 +202,12 @@ window.addEventListener('load', function () {
             img.onerror = () => reject(new Error(`Img undefined: ${imgUrl}`));
         });
     }
+
+    //tab-menu footer
+
+    const tabMenuFooter = [
+        {
+            title: ''
+        },
+    ]
 });
