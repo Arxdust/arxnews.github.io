cb9135d8a1401e4b96b50a3452b51668f8391839
X <sersergiusx@gmail.com>
2021-02-24 16:03:35 +0200

    fix

--- Seafarers/js/render.js
+++ Seafarers/js/render.js
@@ -126,10 +126,9 @@ window.addEventListener('load', function () {
         // comment link
         const commentThisBoxLink = document.querySelector('.comment-this-box a'),
             commentThisBoxLinkSpan = document.createElement('span'),
-            commentTHisBoxLinkSvg = `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="comment-alt" class="svg-inline--fa fa-comment-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M448 0H64C28.7 0 0 28.7 0 64v288c0 35.3 28.7 64 64 64h96v84c0 9.8 11.2 15.5 19.1 9.7L304 416h144c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64z"></path></svg>`;
+            commentTHisBoxLinkSvg = `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="paper-plane" class="svg-inline--fa fa-paper-plane fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"></path></svg>`;
 
-        commentThisBoxLinkSpan.innerText = 'Write a comment';
-
+        commentThisBoxLinkSpan.insertAdjacentText('beforeEnd', 'Write a comment');
         commentThisBoxLink.insertAdjacentHTML('afterBegin', commentTHisBoxLinkSvg);
         commentThisBoxLink.insertAdjacentHTML('beforeEnd', commentThisBoxLinkSpan);
 
