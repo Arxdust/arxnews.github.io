33065eb81c246381570b0cf8be3faeac4d397b31
X <sersergiusx@gmail.com>
2021-02-24 19:17:17 +0200

    fix

---
+++ Seafarers/js/search.js
@@ -0,0 +1,15 @@
+const searchField=document.querySelector("#searchField");
+searchField.addEventListener("keyup", (e)=>{
+    const searchValue = e.target.value;
+    if(searchValue.trim().length>0){
+        console.log("searchValue", searchValue);
+        fetch("search/",{
+            body: JSON.stringify({searchText: searchValue}),
+            method: 'POST',
+        })
+            .then((res)=> res.json())
+            .then((data)=>{
+                console.log("data", data);
+            });
+    }
+});
