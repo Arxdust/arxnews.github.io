d0231f1072e57c105f78f540eaa9ba747a3052fb
X <sersergiusx@gmail.com>
2021-03-08 01:27:30 +0200

    tab menu

--- Seafarers/js/render.js
+++ Seafarers/js/render.js
@@ -136,21 +136,23 @@ window.addEventListener('load', function () {
         // tab-menu-btn
         const btnComment = document.querySelector('.btn-comment'),
             btnCommentSvg = `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="comment-alt" class="svg-inline--fa fa-comment-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M448 0H64C28.7 0 0 28.7 0 64v288c0 35.3 28.7 64 64 64h96v84c0 9.8 11.2 15.5 19.1 9.7L304 416h144c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64z"></path></svg>`,
-            btnShareSvg = ``;
+            btnShareSvg = `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="share" class="svg-inline--fa fa-share fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M503.691 189.836L327.687 37.851C312.281 24.546 288 35.347 288 56.015v80.053C127.371 137.907 0 170.1 0 322.326c0 61.441 39.581 122.309 83.333 154.132 13.653 9.931 33.111-2.533 28.077-18.631C66.066 312.814 132.917 274.316 288 272.085V360c0 20.7 24.3 31.453 39.687 18.164l176.004-152c11.071-9.562 11.086-26.753 0-36.328z"></path></svg>`;
 
         btnComment.insertAdjacentHTML('afterBegin', btnCommentSvg);
 
         // tab-menu
-        $('.tab-menu a').click(function(e){
+        const tabMenu = document.querySelectorAll('.tab-menu a');
+
+        tabMenu.forEach(e => {
             e.preventDefault();
 
-            $(this).parent().siblings().removeClass('active');
-            $(this).parent().addClass('active');
+            e.parent().siblings().removeClass('active');
+            e.parent().addClass('active');
 
-            let tab = $(this).attr('href');
-            $(tab).siblings().removeClass('active');
-            $(tab).addClass('active');
-        });
+            let tab = e.attr('href');
+            e(tab).siblings().removeClass('active');
+            e(tab).addClass('active');
+        })
     }
 
     // checkUrlImg
