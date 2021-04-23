ae20fd92466a342664ba124a8ed4c7da5d7f52dc
arxdust <u.iaroslav@gmail.com>
2021-04-23 23:08:34 +0300

    shabolda

--- Seafarers/js/main.js
+++ Seafarers/js/main.js
@@ -1,3 +1,24 @@
+function addstyle() {
+    const link = document.createElement('link'),
+        body = document.querySelector('.body');
+
+    if (!body.classList.contains('onload')) {
+        body.classList.add('onload');
+    }
+
+    link.rel = 'stylesheet';
+    link.href = 'https://arxdust.github.io/arxnews.github.io/Seafarers/css/style.css';
+    document.head.appendChild(link);
+
+    link.onload = function(){
+        body.classList.add('ready');
+
+        setTimeout(function() {
+            body.classList.remove('onload');
+        }, 200);
+    };
+}
+
 window.addEventListener('load', function() {
     'use strict'
     // logo
@@ -119,6 +140,7 @@ window.addEventListener('load', function() {
     // telegram
     loginTelegram.forEach((elem) => {
         elem.addEventListener('click', toggleShow(popupTelegramContainer));
+        console.log(elem, 'done!');
     })
 
     // main-menu link
