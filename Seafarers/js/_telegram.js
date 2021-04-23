acdf6306e64eda0334d97ec442aa67482bf43b9a
arxdust <u.iaroslav@gmail.com>
2021-04-23 22:29:27 +0300

    shabolda

---
+++ Seafarers/js/_telegram.js
@@ -0,0 +1,11 @@
+windows.addEventListener('load', ()=> {
+    const loginTelegram = document.querySelectorAll('.login-telegram');
+    const popupTelegramContainer = document.querySelector('.popup-telegram-container');
+
+
+    loginTelegram.forEach(elem => {
+        elem.addEventListener('click', ()=> {
+            popupTelegramContainer.classList.toggle('show');
+        })
+    })
+})
