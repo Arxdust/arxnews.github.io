69a7a3afa159f766acbf54029ccfd709092d2224
arxdust <u.iaroslav@gmail.com>
2021-04-23 22:59:10 +0300

    shabolda

--- Seafarers/js/main.js
+++ Seafarers/js/main.js
@@ -1,25 +1,3 @@
-// pre-load css
-function addstyle() {
-    const link = document.createElement('link'),
-        body = document.querySelector('.body');
-
-    if (!body.classList.contains('onload')) {
-        body.classList.add('onload');
-    }
-
-    link.rel = 'stylesheet';
-    link.href = 'https://arxdust.github.io/arxnews.github.io/Seafarers/css/style.css';
-    document.head.appendChild(link);
-
-    link.onload = function(){
-        body.classList.add('ready');
-        
-        setTimeout(function() {
-            body.classList.remove('onload');
-        }, 200);
-    };
-}
-
 window.addEventListener('load', function() {
     'use strict'
     // logo
